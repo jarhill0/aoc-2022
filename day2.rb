1c123af2f7dcd8b6d1122469d4824b94efc73c24
jarhill0 <git@reeshill.net>
2022-12-02 12:13:41 -0600

    Refactor day 2 to use modular arithmetic
    Rather than all the janky hardcoded game outcomes, this is way more
    straightforward.

--- day2.rb
+++ day2.rb
@@ -1,108 +1,35 @@
+# frozen_string_literal: true
+
 require_relative 'input'
 
 class Day2
+  # R=1, P=2, S=3
+  # L=1, D=2, W=3
+  ENCODING = { 'X' => 1, 'Y' => 2, 'Z' => 3,
+               'A' => 1, 'B' => 2, 'C' => 3 }.freeze
+
   def solve
-    INP.lines.map(&:split).map { |l| score(l) }.sum
+    encoded.map { |l| score1(*l) }.sum
   end
 
-  def score(l)
-    opp, me = l
-
-    mp = case me
-         when 'X'
-           1
-         when 'Y'
-           2
-         when 'Z'
-           3
-         end
-
-    wp = case outcome(opp, me)
-         when :win
-           6
-         when :draw
-           3
-         when :lose
-           0
-         end
-
-    mp + wp
-  end
-
-  def outcome(opp, me)
-    opp = { 'A' => 'R', 'B' => 'P', 'C' => 'S' }[opp]
-    me = { 'X' => 'R', 'Y' => 'P', 'Z' => 'S' }[me]
-    if opp == me
-      :draw
-    elsif (opp == 'R' && me == 'S') || (opp == 'S' && me == 'P') || (opp == 'P' && me == 'R')
-      :lose
-    else
-      :win
-    end
-  end
-
-  def score2(l)
-    opp, out = l
-    out = { 'X' => :lose, 'Y' => :draw, 'Z' => :win }[out]
-    me = desired(opp, out)
-
-    mp = case me
-         when 'R'
-           1
-         when 'P'
-           2
-         when 'S'
-           3
-         end
-
-    wp = case out
-         when :win
-           6
-         when :draw
-           3
-         when :lose
-           0
-         end
-
-    mp + wp
-  end
-
-  def desired(opp, out)
-    case opp
-    when 'A' # rock
-      case out
-      when :win
-        'P'
-      when :draw
-        'R'
-      when :lose
-        'S'
-      end
-
-    when 'B' # paper
-      case out
-      when :win
-        'S'
-      when :draw
-        'P'
-      when :lose
-        'R'
-      end
-
-    when 'C' # scissors
-      case out
-      when :win
-        'R'
-      when :draw
-        'S'
-      when :lose
-        'P'
-      end
-    end
+  def score1(opponent, me)
+    outcome = (me - opponent + 1) % 3
+    me + 3 * outcome
   end
 
   def solve2
-    INP.lines.map(&:split).map { |l| score2(l) }.sum
+    encoded.map { |l| score2(*l) }.sum
+  end
+
+  def score2(opponent, outcome)
+    me = (opponent + outcome) % 3 + 1
+    me + 3 * (outcome - 1)
+  end
+
+  private
+
+  def encoded
+    INP.lines.map(&:split).map { |l| l.map { |m| ENCODING[m] } }
   end
 end
 

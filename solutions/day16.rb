e0ce1aa994dba603ab1203c96667dd216ee0695c
jarhill0 <git@reeshill.net>
2022-12-22 22:41:12 -0800

    Day 16 part 1
    I finally solved it in a reasonable time by taking my original recursive
    solution and simply caching all of the distances instead of doing a BFS
    every single time. Who knew?

--- solutions/day16.rb
+++ solutions/day16.rb
@@ -8,7 +8,7 @@ class Day16 < Solution
   P1_MINUTES = 30
 
   def solve
-    greedy
+    best_flow
   end
 
   def valves
@@ -27,39 +27,6 @@ class Day16 < Solution
     end.freeze
   end
 
-  def greedy
-    location = 'AA'
-    minutes_remaining = P1_MINUTES
-    valves_open = Set.new
-    total_flow = 0
-
-    while (valve, travel_time = next_valve(location, minutes_remaining, valves_open))
-      valves_open << valve
-      minutes_remaining -= travel_time
-      location = valve
-      total_flow += minutes_remaining * flow_rate(valve)
-
-      puts "== Minute #{P1_MINUTES - minutes_remaining} =="
-      puts "You open valve #{valve}."
-    end
-
-    total_flow
-  end
-
-  def next_valve(location, minutes_remaining, valves_open)
-    all_options = valve_options(valves_open)
-                  .map { |vn| [vn, distance(location, vn) + 1] }
-                  .filter { |_, enable_time| enable_time < minutes_remaining }
-    p(all_options.map do |valve_name, enable_time|
-      potential_gain = flow_rate(valve_name) * (minutes_remaining - enable_time)
-      [valve_name, potential_gain, enable_time, potential_gain / enable_time]
-    end)
-    all_options.max_by do |valve_name, enable_time|
-      potential_gain = flow_rate(valve_name) * (minutes_remaining - enable_time)
-      potential_gain / enable_time
-    end
-  end
-
   def flow_rate(valve_name)
     valves[valve_name][:rate]
   end
@@ -72,7 +39,6 @@ class Day16 < Solution
     return total_so_far if opts.length.zero?
 
     opts.map do |name|
-      flow = valves[name][:rate]
       minutes_busy = distance(current_location, name) + 1
 
       next total_so_far if minutes_busy > minutes_remaining
@@ -81,7 +47,7 @@ class Day16 < Solution
         current_location: name,
         minutes_remaining: minutes_remaining - minutes_busy,
         valves_open: (valves_open + Set[name]).freeze,
-        total_so_far: total_so_far + (flow * (minutes_remaining - minutes_busy))
+        total_so_far: total_so_far + (flow_rate(name) * (minutes_remaining - minutes_busy))
       )
     end.max
   end
@@ -95,6 +61,11 @@ class Day16 < Solution
   end
 
   def distance(start, end_)
+    @distance ||= {}
+    @distance[Set[start, end_].freeze] ||= bfs_distance(start, end_)
+  end
+
+  def bfs_distance(start, end_)
     return 0 if start == end_
 
     visited = Set[start]

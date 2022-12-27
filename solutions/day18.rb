01ad3605166344abfd798ed50a444e4c579a8429
jarhill0 <git@reeshill.net>
2022-12-27 14:45:41 -0500

    Day 18 part 1

---
+++ solutions/day18.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require_relative 'input'
+require_relative 'solution'
+require 'set'
+
+class Day18 < Solution
+  def solve
+    cubes.map do |cube1|
+      6 - covered_faces(cube1)
+    end.sum
+  end
+
+  def cubes
+    @cubes ||= INP.lines.map { |l| l.split(',').map(&:to_i).freeze }.freeze
+  end
+
+  def covered_faces(cube1)
+    cubes.filter { |c| cube1 != c }.count do |cube2| # filter may be unnecessary
+      shares_face(cube1, cube2)
+    end
+  end
+
+  def shares_face(cube1, cube2)
+    coord_differences = cube1.zip(cube2).map { |coord1, coord2| (coord1 - coord2).abs }
+    coord_differences.count(1) == 1 && coord_differences.count(0) == 2 # Set[coord_differences] == Set[1, 0, 0]
+  end
+
+  def solve2; end
+end
+
+Day18.new.execute

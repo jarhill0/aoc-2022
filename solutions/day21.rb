e86a40b05f2b7f6bed0fa747dcdd697cae3f05e5
jarhill0 <git@reeshill.net>
2022-12-28 00:09:05 -0500

    Day 21 part 2
    This was super fun! I could have plugged it into WolframAlpha or
    something, but it was actually quite simple to go down the tree with the
    crucial assumption that "humn" was only called once (which must be true
    for this linear equation to be solveable, since we only have one equation).
    
    At each monkey, we have a desired value. One branch gives a constant and
    the other contains the human. We have a very simple linear equation of
    one variable which we can manually invert. Then we know what we want the
    human-containing branch to equal, so we tail-recurse on that.

--- solutions/day21.rb
+++ solutions/day21.rb
@@ -22,7 +22,96 @@ class Day21 < Solution
     end.freeze
   end
 
-  def solve2; end
+  def p2_monkeys
+    @p2_monkeys ||= INP.lines.to_h do |line|
+      name, operation = line.split(': ')
+      if name == 'humn'
+        [name, nil]
+      elsif /\d/.match?(operation)
+        [name, operation.to_i]
+      else
+        [name, operation.split]
+      end
+    end.freeze
+  end
+
+  def solve2
+    root = p2_monkeys['root']
+    left = evaluate(root.first)
+    right = evaluate(root.last)
+
+    desired = left || right
+    find_human_value(left.nil? ? root.first : root.last, desired)
+  end
+
+  def evaluate(monkey_name)
+    return nil if monkey_name == 'humn'
+
+    monkey = p2_monkeys[monkey_name]
+
+    if monkey.is_a?(Integer)
+      monkey
+    else
+      left, operation, right = monkey
+      left_result = evaluate(left)
+      right_result = evaluate(right)
+
+      left_result && right_result && left_result.send(operation, right_result)
+    end
+  end
+
+  def find_human_value(monkey_name, desired)
+    return desired if monkey_name == 'humn'
+
+    monkey = p2_monkeys[monkey_name]
+
+    raise 'No human value for Integer monkey!' if monkey.is_a?(Integer)
+
+    left, operation, right = monkey
+    left_result = evaluate(left)
+    right_result = evaluate(right)
+
+    monkey_name = left_result.nil? ? left : right
+
+    desired =
+      if left_result.nil?
+        # left_expr OP right_result = desired
+        # => left_expr = desired OP_INV right_result
+        case operation
+        when '+'
+          desired - right_result
+        when '-'
+          desired + right_result
+        when '*'
+          desired / right_result
+        when '/'
+          desired * right_result
+        else
+          raise "Unknown operation #{operation}"
+        end
+      else
+
+        case operation
+        when '+'
+          desired - left_result
+        when '-'
+          # left_result - right_expr = desired
+          # => -right_expr = desired - left_result
+          # => right_expr = left_result - desired
+          left_result - desired
+        when '*'
+          desired / left_result
+        when '/'
+          # left_result / right_expr = desired
+          # => left_result = right_expr * desired
+          # => left_result / desired = right_expr
+          left_result / desired
+        else
+          raise "Unknown operation #{operation}"
+        end
+      end
+    find_human_value(monkey_name, desired)
+  end
 end
 
 Day21.new.execute

7039c445754a36e5b0350ea2873556ef35bd74bb
jarhill0 <git@reeshill.net>
2022-12-28 12:00:21 -0500

    Start day 22 part 2
    Most people online did not do a general solution here, but I'm
    interested in the challenge.

--- solutions/day22.rb
+++ solutions/day22.rb
@@ -102,9 +102,91 @@ class Day22 < Solution
     (1000 * (@row + 1)) + (4 * (@col + 1)) + DIRECTIONS.index(@dir)
   end
 
-  def solve2; end
+  def solve2
+    find_face_bounds
+  end
+
+  def walk2
+    update_visualization
+    path.each do |instruction|
+      turn(instruction) and next if instruction.is_a?(Symbol)
+
+      instruction.times do
+        next_pos, next_dir = one_cube_step_forward
+        break if wall?(next_pos)
+
+        @row, @col = next_pos
+        @dir = next_dir
+        update_visualization
+      end
+    end
+  end
+
+  def one_cube_step_forward
+    delta = DELTAS[@dir]
+    new_row, new_col = [@row, @col].zip(delta).map(&:sum)
+    new_dir = @dir
+
+    [[new_row, new_col], new_dir]
+  end
+
+  def face_bounds
+    @face_bounds ||= find_face_bounds
+  end
 
   private
+
+  # I could hard-code this for my input, but I like the challenge of solving the general case
+  # Also, that helps me test on the sample input.
+  def find_face_bounds
+    # chunk vertically
+    row_0_bounds = map.first[0]
+
+    vertical_chunks = []
+    first_row_this_chunk = 0
+    curr_l = row_0_bounds[1] - row_0_bounds[0] + 1
+    map.each_with_index do |row, row_ind|
+      bounds = row[0]
+      l = bounds[1] - bounds[0] + 1
+
+      next unless l != curr_l
+
+      vertical_chunks << [first_row_this_chunk, row_ind - 1]
+      curr_l = l
+      first_row_this_chunk = row_ind
+    end
+
+    vertical_chunks << [first_row_this_chunk, map.length - 1]
+
+    # find face width from vertical chunks
+    face_width = vertical_chunks.map(&:first).map do |row_ind|
+      bounds = map[row_ind][0]
+      bounds[1] - bounds[0] + 1
+    end.min
+
+    # now use that to divide the larger chunks
+    faces = vertical_chunks.flat_map do |start_row_ind, end_row_ind|
+      bounds = map[start_row_ind][0]
+      width = bounds[1] - bounds[0] + 1
+      num_faces = width / face_width
+
+      num_faces.times.map do |i|
+        start_col_ind = bounds[0] + (i * face_width)
+        end_col_ind = bounds[0] + ((i + 1) * face_width) - 1
+        [[start_row_ind, start_col_ind].freeze, [end_row_ind, end_col_ind].freeze].freeze
+      end
+    end.freeze
+
+    warn(faces.inspect)
+
+    # TODO: now figure out how they connect
+    # connect immediate neighbors by comparing coordinates
+
+    # fill in the rest by aggregating transitions over the existing connections until all faces have all connections
+    # (that is, repeatedly translate A->B->C into A->C, until the connections are filled)
+
+    # me[up] = me[up][right] + 1 turn to the right
+  end
 
   def last_facing
     @last_facing ||= {}

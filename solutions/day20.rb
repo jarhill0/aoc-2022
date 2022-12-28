1f9171a5fc5176d5586d6a25716cc190be478d93
jarhill0 <git@reeshill.net>
2022-12-27 23:36:17 -0500

    Day 20 part 2
    Dead simple. I just had to pull out the mixing stuff into its own
    method, and reroute all lookups of the original values into a new
    method, where I could "decrypt" values on demand. The large numbers came
    out in the wash.
    
    I think that I missed the naïve solution here entirely. Based on this, I
    suppose the naïve version uses linked lists and doesn't mod out the
    max-5000 displacement. But even with linked lists, I think part 2 would
    be straightforward by modding out the difference.

--- solutions/day20.rb
+++ solutions/day20.rb
@@ -6,23 +6,31 @@ require 'set'
 
 class Day20 < Solution
   def solve
-    (0...inp_sequence.length).each do |original_index|
-      shuffle(original_index)
-    end
+    mix
 
     coordinates
+  end
+
+  def mix
+    (0...input_length).each do |original_index|
+      shuffle(original_index)
+    end
   end
 
   def print
     puts indices.to_s
     puts(indices.map do |original_index|
-      inp_sequence[original_index]
+      value_at_original_index(original_index)
     end.join(', '))
   end
 
   def indices
     # list of indices into the original list, now in a new order.
-    @indices ||= (0...inp_sequence.length).to_a
+    @indices ||= (0...input_length).to_a
+  end
+
+  def reset
+    @indices = nil
   end
 
   def inp_sequence
@@ -34,7 +42,7 @@ class Day20 < Solution
   end
 
   def shuffle(original_index)
-    value = inp_sequence[original_index]
+    value = value_at_original_index(original_index)
     current_index = indices.find_index(original_index) # TODO: O(n) -- augment with a Hash?
 
     move(current_index, value)
@@ -49,7 +57,15 @@ class Day20 < Solution
   end
 
   def value_at(index)
-    inp_sequence[indices[wrap_lookup(index)]]
+    value_at_original_index(indices[wrap_lookup(index)])
+  end
+
+  def value_at_original_index(original_index)
+    if @decrypt
+      DECRYPTION_KEY * inp_sequence[original_index]
+    else
+      inp_sequence[original_index]
+    end
   end
 
   def delete_at(index)
@@ -70,13 +86,19 @@ class Day20 < Solution
 
   def coordinates
     zero_ind = indices.each_with_index.find do |inp_index, _current_index|
-      inp_sequence[inp_index].zero?
+      value_at_original_index(inp_index).zero?
     end.last
 
     value_at(zero_ind + 1000) + value_at(zero_ind + 2000) + value_at(zero_ind + 3000)
   end
 
-  def solve2; end
+  DECRYPTION_KEY = 811_589_153
+  def solve2
+    reset
+    @decrypt = true
+    10.times { mix }
+    coordinates
+  end
 end
 
 Day20.new.execute

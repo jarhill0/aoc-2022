dc25b55cbe99d9e9b6ced8d11d723455af932e8d
jarhill0 <git@reeshill.net>
2022-12-28 23:05:58 -0500

    Day 16 part 2!
    I read a blog post of someone else's solution, and their approach gave
    me the insight I needed (aka I used their approach).
    
    The crux of it was: build a map, where the key is the valves opened in
    26 minutes, and the value is the best flow achieved with that
    combination of valves. From that, you can determine which disjoint pair
    of valve sets has the best total flow.
    
    <https://nickymeuleman.netlify.app/garden/aoc2022-day16#part-2>

--- solutions/day16.rb
+++ solutions/day16.rb
@@ -37,7 +37,7 @@ class Day16 < Solution
     valves_open: Set.new.freeze,
     total_so_far: 0
   )
-    return 0 if minutes_remaining <= 0
+    raise 'Too little time!' if minutes_remaining <= 0
 
     opts = valve_options(valves_open)
 
@@ -46,7 +46,7 @@ class Day16 < Solution
     opts.map do |name|
       minutes_busy = distance(current_location, name) + 1
 
-      next total_so_far if minutes_busy > minutes_remaining
+      next total_so_far if minutes_busy >= minutes_remaining
 
       best_flow(
         current_location: name,
@@ -99,7 +99,51 @@ class Day16 < Solution
 
   P2_MINUTES = 26
 
-  def solve2; end
+  def solve2
+    valve_options = {}
+    flow_from_valves(valve_map: valve_options)
+    best_combination(valve_options)
+  end
+
+  def best_combination(valve_options)
+    valve_options.filter_map do |my_valves, my_total|
+      valve_options.filter_map do |elephant_valves, elephant_total|
+        next unless my_valves.disjoint?(elephant_valves)
+
+        my_total + elephant_total
+      end.max
+    end.max
+  end
+
+  def flow_from_valves(
+    valve_map:,
+    current_location: 'AA',
+    minutes_remaining: P2_MINUTES,
+    valves_open: Set.new.freeze,
+    total_so_far: 0
+  )
+    raise 'Too little time!' if minutes_remaining <= 0
+
+    valve_map[valves_open] = total_so_far if !valve_map[valves_open] || valve_map[valves_open] < total_so_far
+
+    opts = valve_options(valves_open)
+
+    return if opts.empty?
+
+    opts.each do |name|
+      minutes_busy = distance(current_location, name) + 1
+
+      next if minutes_busy >= minutes_remaining
+
+      flow_from_valves(
+        current_location: name,
+        minutes_remaining: minutes_remaining - minutes_busy,
+        valves_open: (valves_open + Set[name]).freeze,
+        total_so_far: total_so_far + (flow_rate(name) * (minutes_remaining - minutes_busy)),
+        valve_map:,
+      )
+    end
+  end
 end
 
 Day16.new.execute
